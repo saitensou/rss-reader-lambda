46b0c715eb77962e3882007df2a48dbd083ad4b4
seto seto <setoseto@saitensou.com>
2022-10-12 19:39:42 +0800

    add api gateway

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -3,10 +3,11 @@ import aws_cdk as cdk
 import aws_cdk.aws_dynamodb as DynamoDB
 import aws_cdk.aws_lambda as Lambda
 import aws_cdk.aws_logs as Logs
+import aws_cdk.aws_apigateway as APIgateway
 from constructs import Construct
 
-class CdkStack(cdk.Stack):
 
+class CdkStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
@@ -15,8 +16,12 @@ class CdkStack(cdk.Stack):
             self,
             "rss_origin_table",
             table_name="rss_origin_table",
-            partition_key=DynamoDB.Attribute(name="siteURL", type=DynamoDB.AttributeType.STRING),
-            sort_key=DynamoDB.Attribute(name="siteName", type=DynamoDB.AttributeType.STRING),
+            partition_key=DynamoDB.Attribute(
+                name="siteURL", type=DynamoDB.AttributeType.STRING
+            ),
+            sort_key=DynamoDB.Attribute(
+                name="siteName", type=DynamoDB.AttributeType.STRING
+            ),
             billing_mode=DynamoDB.BillingMode.PAY_PER_REQUEST,
         )
 
@@ -24,47 +29,63 @@ class CdkStack(cdk.Stack):
             self,
             "rss_feed_item_table",
             table_name="rss_feed_item_table",
-            partition_key=DynamoDB.Attribute(name="link", type=DynamoDB.AttributeType.STRING),
-            sort_key=DynamoDB.Attribute(name="published", type=DynamoDB.AttributeType.STRING),
+            partition_key=DynamoDB.Attribute(
+                name="link", type=DynamoDB.AttributeType.STRING
+            ),
+            sort_key=DynamoDB.Attribute(
+                name="published", type=DynamoDB.AttributeType.STRING
+            ),
             billing_mode=DynamoDB.BillingMode.PAY_PER_REQUEST,
         )
 
         # layers
         feedparser_layer = Lambda.LayerVersion(
-            self, "feedparser_layer",
+            self,
+            "feedparser_layer",
             removal_policy=cdk.RemovalPolicy.RETAIN,
-            code=Lambda.Code.from_asset('lambda-layer'),
-            compatible_runtimes=[Lambda.Runtime.PYTHON_3_9]
+            code=Lambda.Code.from_asset("lambda-layer"),
+            compatible_runtimes=[Lambda.Runtime.PYTHON_3_9],
         )
 
         # functions
         rss_fetch_lambda = Lambda.Function(
-            self, 'rss_fetch_lambda',
+            self,
+            "rss_fetch_lambda",
             runtime=Lambda.Runtime.PYTHON_3_9,
             function_name="rss_fetch_lambda",
-            code=Lambda.Code.from_asset('lambda'),
-            handler='fetch_feed_handler.handler',
+            code=Lambda.Code.from_asset("lambda"),
+            handler="fetch_feed_handler.handler",
             log_retention=Logs.RetentionDays.ONE_WEEK,
             timeout=cdk.Duration.minutes(5),
             environment={
                 "RSS_ORIGIN_TABLE": rss_origin_table.table_name,
-                "RSS_FEED_ITEM_TABLE": rss_feed_item_table.table_name
+                "RSS_FEED_ITEM_TABLE": rss_feed_item_table.table_name,
             },
-            layers=[feedparser_layer]
+            layers=[feedparser_layer],
         )
         rss_origin_table.grant_read_data(rss_fetch_lambda)
         rss_feed_item_table.grant_read_write_data(rss_fetch_lambda)
 
         rss_serve_lambda = Lambda.Function(
-            self, 'rss_serve_lambda',
+            self,
+            "rss_serve_lambda",
             runtime=Lambda.Runtime.PYTHON_3_9,
             function_name="rss_serve_lambda",
-            code=Lambda.Code.from_asset('lambda'),
-            handler='serve_feed_handler.handler',
+            code=Lambda.Code.from_asset("lambda"),
+            handler="serve_feed_handler.handler",
             log_retention=Logs.RetentionDays.ONE_WEEK,
             timeout=cdk.Duration.seconds(10),
-            environment={
-                "RSS_FEED_ITEM_TABLE": rss_feed_item_table.table_name
-            }
+            environment={"RSS_FEED_ITEM_TABLE": rss_feed_item_table.table_name},
         )
         rss_feed_item_table.grant_read_data(rss_serve_lambda)
+
+        # API gateway
+        saitensou_api = APIgateway.LambdaRestApi(
+            self,
+            "saitensou-rss-api-service",
+            handler=rss_serve_lambda,
+            proxy=False,
+            rest_api_name="saitensou-rss-api-service",
+        )
+        saitensou_api_get_rss = saitensou_api.root.add_resource("rss")
+        saitensou_api_get_rss.add_method("GET")
--- lambda/serve_feed_handler.py
+++ lambda/serve_feed_handler.py
@@ -1,17 +1,19 @@
+import json
 import boto3
 import os
 
-feed_item_table_name = os.environ['RSS_FEED_ITEM_TABLE']
-dynamodb = boto3.resource('dynamodb')
+feed_item_table_name = os.environ["RSS_FEED_ITEM_TABLE"]
+dynamodb = boto3.resource("dynamodb")
+
 
 def scanTable(table):
     response = table.scan()
-    data = response['Items']
+    data = response["Items"]
 
-    while 'LastEvaluatedKey' in response:
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
-    
+    while "LastEvaluatedKey" in response:
+        response = table.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
+        data.extend(response["Items"])
+
     return data
 
 
@@ -19,6 +21,11 @@ def handler(event, context):
     feed_item_table = dynamodb.Table(feed_item_table_name)
 
     result = scanTable(feed_item_table)
-    print(result)
+    json_result = json.dumps(result)
+    print(json_result)
 
-    return result
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json_result,
+    }
